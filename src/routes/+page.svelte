fce175a9e99b751e88a919e0074532a8cbd0767e
raynecloudy <rayne@raynec.dev>
2025-06-07 20:17:29 -0700

    hello world

---
+++ src/routes/+page.svelte
@@ -0,0 +1,243 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+
+  // @ts-ignore
+  let picture: HTMLDivElement = $state();
+  // @ts-ignore
+  let main: HTMLElement = $state();
+  let loaded = $state(false);
+  
+  onMount(() => {
+    loaded = true;
+  });
+</script>
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Mukta+Mahee:wght@200;300;400;500;600;700;800&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap");
+
+  :global(body) {
+    color: #581ec2;
+    background-image: linear-gradient(-15deg, #fbf0ff, #fff0fb);
+    text-shadow: 0.1rem 0.1rem 0 #ffc5f5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+    padding: 3rem;
+    width: calc(100vw - 6rem);
+    height: calc(100vh - 6rem);
+    font-family: "Mukta Mahee";
+  }
+
+  ::selection {
+    text-shadow: none;
+    background-color: #ffc5f5;
+    color: #581ec2;
+  }
+
+  :focus-visible {
+    outline: 0.15rem solid #581ec2;
+  }
+  
+  main {
+    display: flex;
+    gap: 5rem;
+    position: relative;
+    align-items: center;
+    outline: 0.15rem solid #ffc5f5;
+    padding: 3.5rem;
+    border-radius: 7rem;
+    box-shadow: 0.3rem 0.3rem 0 #ffc5f5;
+  }
+
+  #info {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  #info * {
+    margin: 0;
+  }
+
+  div:has(> #picture) {
+    width: 17rem;
+    height: 20rem;
+    border-radius: 4rem;
+    background-color: #ffc5f5;
+    overflow: hidden;
+  }
+
+  #picture {
+    width: inherit;
+    height: inherit;
+    border-radius: inherit;
+    background-image: url("/rayne.png");
+    background-color: #fcf8ff;
+    background-size: 25rem;
+    position: relative;
+    top: 0.3rem;
+    left: 0.3rem;
+    background-repeat: no-repeat;
+  }
+
+  #social-bar {
+    display: flex;
+    /* padding-top: 1rem; */
+    /* gap: 1.2rem; */
+    /* outline: 0.15rem solid #ffc5f5; */
+    border-radius: 1rem;
+    overflow: hidden;
+  }
+
+  #social-bar a {
+    display: block;
+    user-select: none;
+    line-height: 0rem;
+    padding: 0.8rem;
+    flex-grow: 1;
+    text-align: center;
+  }
+
+  #social-bar img {
+    height: 1.4rem;
+  }
+
+  #social-bar a:not(:not(:hover, :focus-visible)),
+  .light-up {
+    outline: none;
+    background-color: #ffc5f5;
+  }
+
+  main content {
+    width: 35rem;
+    max-height: 30rem;
+    overflow: scroll;
+  }
+
+  select {
+    padding: 0.5rem 1rem;
+    font: inherit;
+    border: none;
+    margin: 0;
+    color: inherit;
+    text-shadow: inherit;
+    background-color: transparent;
+    outline: 0.15rem solid #ffc5f5;
+    border-radius: 1rem;
+    cursor: pointer;
+  }
+
+  #donator-list {
+    list-style: "❤ ";
+  }
+
+  .rainbow-donator {
+    animation: rainbow 4s infinite linear forwards;
+  }
+
+  .rainbow-donator::marker {
+    color: #ff0000;
+  }
+
+  .rainbow-donator a {
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-image: linear-gradient(90deg, #ff0095, #ffbb00);
+    color: #ff9100;
+    text-shadow: none;
+    font-weight: bold;
+  }
+
+  @keyframes rainbow {
+    0% {
+      filter: hue-rotate(0deg);
+    }
+
+    100% {
+      filter: hue-rotate(360deg);
+    }
+  }
+
+  @media (max-width: 70rem) {
+    :global(body) {
+      display: block;
+      padding: 0;
+    }
+
+    main {
+      flex-direction: column;
+      width: calc(100% - 1rem);
+      position: static;
+      border-radius: 0;
+      box-shadow: none;
+      outline: none;
+    }
+
+    #info,
+    main content {
+      width: 100%;
+    }
+
+    div:has(> #picture) {
+      width: 100%;
+    }
+
+    #picture {
+      background-position: 50% 0 !important;
+    }
+
+    main content {
+      max-height: unset;
+    }
+  }
+</style>
+
+<svelte:body onmousemove={(event) => {
+  picture.style.backgroundPosition = `${(event.clientX / 80) - 60}px ${(event.clientY / 80) - 35}px`;
+  main.style.left = `${(event.clientX / -40)}px`;
+  main.style.top = `${(event.clientY / -40)}px`;
+}}></svelte:body>
+
+{#if loaded}
+<main in:fade bind:this={main}>
+  <div id="info">
+    <div>
+      <div id="picture" bind:this={picture}></div>
+    </div>
+    <h2>rayne cloudy</h2>
+    <div id="social-bar">
+      <a href="https://github.com/raynecloudy"><img src="/GitHub_Invertocat_Dark.svg" alt="github"></a>
+      <a href="https://bsky.app/profile/raynec.dev"><img src="/bsky.svg" alt="bluesky"></a>
+      <a href="https://discord.gg/mD6metDHNE"><img src="/Discord-Symbol-Black.svg" alt="discord"></a>
+      <a href="https://patreon.com/raynecloudy" class={page.url.hash === "#donators" ? "light-up" : ""}><img src="/PATREON_SYMBOL_1_BLACK_RGB.svg" alt="patreon"></a>
+    </div>
+    <select bind:value={page.url.hash} onchange={(event) => location.hash = event.currentTarget.value}>
+      <option value="" selected>about</option>
+      <option value="#donators">donators</option>
+      <option disabled>coming soon....</option>
+    </select>
+  </div>
+  <content>
+    {#if page.url.hash === ""}
+    <h1>about</h1>
+    <p>he/she</p>
+    <p>hii!! my names rayne!!</p>
+    <p>i'm a teenage computer scientist on the internet. i've been programming for eight years and have had an interest in web development for five years. i really like music, mexican food, and typescript.</p>
+    <p>i started programming when i was seven years old. i've come a long way since then!! the first programming language i learned was python, and i'm now fluent in four more: javascript, typescript, rust, and C++!</p>
+    {:else if page.url.hash === "#donators"}
+    <h1>patreons</h1>
+    <p>thank you to all the people who've donated to me!!!! it makes my day to know people enjoy what i make &lt;3</p>
+    <ul id="donator-list">
+      <li class="rainbow-donator"><a href="https://dumorando.com">dumorando</a></li>
+      <li><a href="https://samv.me">shock59</a></li>
+    </ul>
+    {:else}
+    <h1>404</h1>
+    <p>this page could not be found</p>
+    {/if}
+  </content>
+</main>
+{/if}
