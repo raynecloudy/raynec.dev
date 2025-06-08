94d4443f9b1c1dd2f65eeee20471ec5d43c8b57b
raynecloudy <rayne@raynec.dev>
2025-06-08 12:30:21 -0700

    silly battle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -251,7 +251,8 @@
       <option value="#donators">donators</option>
       <option value="#friends">friends</option>
       <option value="#socials">socials</option>
-      <option disabled>coming soon....</option>
+      <option disabled>----- extras -----</option>
+      <option value="#puns">rayne and eliza's pun battle</option>
     </select>
   </div>
   <content>
@@ -303,6 +304,41 @@
           <li><a href="https://patreon.com/raynecloudy">patreon</a></li>
           <li><a href="https://pronouns.cc/@raynec">pronouns.cc</a></li>
         </ul>
+      </div>
+    </container>
+    {:else if page.url.hash === "#puns"}
+    <container>
+      <div in:fly={{ y: "1rem", opacity: 1 }}>
+        <h1>rayne and eliza's epic pun battle!!</h1>
+        <p>me and my friend eliza had a pun battle in discord DMs. with permission, i've put it here so everybody can see these cheesy computer science puns :3</p>
+        <p><i>eliza:&nbsp;</i>anyway i'm <a href="https://en.wikipedia.org/wiki/Switch_statement">switch</a>ing [these if statements] to a switch/case statement now</p>
+        <p><i>rayne:&nbsp;</i>i see what you did there (probably because you bolded it)</p>
+        <p><i>eliza:&nbsp;</i>yeah, i bolded it just in <a href="https://en.wikipedia.org/wiki/Switch_statement">case</a></p>
+        <p><i>rayne:&nbsp;</i>oh my goodness you did it twice</p>
+        <p><i>eliza:&nbsp;</i>yeah, these puns are getting pretty bad, i should stop, or <a href="https://en.wikipedia.org/wiki/Conditional_(computer_programming)#If%E2%80%93then(%E2%80%93else)">else</a> i'll run out of puns to make</p>
+        <p><i>rayne:&nbsp;</i><a href="https://en.wikipedia.org/wiki/Conditional_(computer_programming)#If%E2%80%93then(%E2%80%93else)">if</a> you make one more i'm going to <a href="https://en.wikipedia.org/wiki/Control_flow#Early_exit_from_loops">break</a> your face</p>
+        <p><i>eliza:&nbsp;</i>ooh that was a good one, but maybe we should <a href="https://en.wikipedia.org/wiki/Branching_(version_control)">branch</a> out a little more</p>
+        <p><i>rayne:&nbsp;</i>don't be <a href="https://en.wikipedia.org/wiki/Push_technology">push</a>y about it</p>
+        <p><i>eliza:&nbsp;</i>oh <a href="https://en.wikipedia.org/wiki/For_loop">for</a> goodness sakes, i guess we've really <a href="https://en.wikipedia.org/wiki/Commit_(version_control)">commit</a>ted to the bit now, haven't we</p>
+        <p><i>rayne:&nbsp;</i>these jokes are so bad i may have to leave and <a href="https://en.wikipedia.org/wiki/Return_statement">return</a> when you're through. it's becoming an <a href="https://en.wikipedia.org/wiki/Software_project_management#Issue">issue</a></p>
+        <p><i>eliza:&nbsp;</i>i <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</a> i'll stop eventually</p>
+        <p><i>rayne:&nbsp;</i>i'll be <a href="https://en.wikipedia.org/wiki/Wait_(command)">wait</a>ing <a href="https://www.lua.org/pil/4.3.3.html">until</a> you run out</p>
+        <p><i>eliza:&nbsp;</i>you might be waiting a <a href="https://en.wikipedia.org/wiki/While_loop">while</a></p>
+        <p><i>rayne:&nbsp;</i>i may have to <a href="https://en.wikipedia.org/wiki/Exit_(command)">exit</a> this chat and <a href="https://en.wikipedia.org/wiki/Async/await">await</a> your exhaustion</p>
+        <p><i>eliza:&nbsp;</i>i'm sure we could <a href="https://en.wikipedia.org/wiki/Array_(data_structure)">array</a>nge for that</p>
+        <p><i>rayne:&nbsp;</i>i'll have plenty of things to <a href="https://carpentry.library.ucsb.edu/2021-01-21-SWC-Bash-online/05-loop/index.html">do</a> outside</p>
+        <p><i>eliza:&nbsp;</i><a href="https://en.wikipedia.org/wiki/AND_gate">and</a> i'm sure touching grass would be good for you</p>
+        <p><i>rayne:&nbsp;</i>oh, you're gonna be like that, huh? &gt;:&lpar; take that back <a href="https://en.wikipedia.org/wiki/OR_gate">or</a> i will <a href="https://en.wikipedia.org/wiki/Make_(software)">make</a> mincemeat out of your face</p>
+        <p><i>eliza:&nbsp;</i>hey, <a href="https://en.wikipedia.org/wiki/This_(computer_programming)">this</a> doesn't have to <a href="https://www.lua.org/pil/4.2.html">end</a> like that</p>
+        <p><i>rayne:&nbsp;</i>you may want to take a <a href="https://en.wikipedia.org/wiki/Class_(computer_programming)">class</a> on being nice to people, we're more than <a href="https://en.wikipedia.org/wiki/Object_(computer_science)">object</a>s for you to be <a href="https://en.wikipedia.org/wiki/Using#Programming_language_keywordsc">using</a></p>
+        <p><i>eliza:&nbsp;</i>you may need to include yourself in that class too. you might <a href="https://www.w3schools.com/nodejs/nodejs_modules.asp">require</a> some anger management classes too if you're just going to <a href="https://en.wikipedia.org/wiki/Run_command">run</a> around threatening to punch peoples' faces <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in">in</a></p>
+        <p><i>rayne:&nbsp;</i>if any<a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/body">body</a> here needs to <a href="https://en.wikipedia.org/wiki/Touch_(command)">touch</a> grass, it's you</p>
+        <p><i>eliza:&nbsp;</i>not to be too <a href="https://en.wikipedia.org/wiki/Strict">strict</a> here, but since when was touch a coding word? i don't re<a href="https://en.wikipedia.org/wiki/System_call">call</a> that being part of any language i know</p>
+        <p><i>rayne:&nbsp;</i>you'll find it's a bash command for creating files, much like i'm going to <a href="https://en.wikipedia.org/wiki/Bash_(Unix_shell)">bash</a> your <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/head">head</a> off</p>
+        <p><i>eliza:&nbsp;</i>woah, no need to over<a href="https://en.wikipedia.org/wiki/React_(software)">react</a></p>
+        <p><i>rayne:&nbsp;</i>you <a href="https://en.wikipedia.org/wiki/Const_(computer_programming)">constant</a>ly annoy me. i may have to ask you to leave my <a href="https://en.wikipedia.org/wiki/Property_(programming)">property</a> if this <a href="https://en.wikipedia.org/wiki/Control_flow#Continuation_with_next_iteration">continue</a>s</p>
+        <p><i>eliza:&nbsp;</i>i hate to call this off early, but my <a href="https://en.wikipedia.org/wiki/Inheritance_(object-oriented_programming)">parent</a>s are telling me i have to shower. i guess we'll have to return to this later</p>
+        <p><i>rayne:&nbsp;</i>i would hope <a href="https://en.wikipedia.org/wiki/Inverter_(logic_gate)">not</a></p>
       </div>
     </container>
     {:else}
