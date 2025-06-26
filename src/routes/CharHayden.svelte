48b411dc9395b237e394db164086389b28dd06d2
raynecloudy <rayne@raynec.dev>
2025-06-25 18:47:01 -0700

    hayden yay

--- src/routes/CharHayden.svelte
+++ src/routes/CharHayden.svelte
@@ -1,2 +1,27 @@
-<h1><span title="first">hayden</span></h1>
-<p>this character ref is under construction, come back later!</p>
+<div class="side">
+  <div>
+    <h1><span title="first">hayden</span></h1>
+    <h2>lore</h2>
+    <p>hayden had a very severe identity crisis at the age of 14. he just felt unhappy with himself for no identifiable reason. it became so severe that a new personality was formed, and took the form of a humanoid entity which was named "hayley". <a href="#char-hayley">hayley</a> offered hayden a chance at being happy with himself, which he accepted. through a magic process conducted by hayley, the boy and the girl merged into one entity, <a href="#char-rayne">rayne</a>.</p>
+  </div>
+  <img src="https://stuff.raynec.dev/art/2025/hayden%20remastered.png" alt="hayden">
+</div>
+<p>(the kid was having gender issues. LOL)</p>
+<h2>appearance</h2>
+<h3>physical</h3>
+<p>he's 5' 9".</p>
+<p>his hair is a light coffee brown color.</p>
+<p>his eyes are orange.</p>
+<p>he is white.</p>
+<h3>cosmetic</h3>
+<p>feel free to draw hayden in any outfit that suits his personality, but if you want it, here's his canon outfit:</p>
+<ol>
+  <li>orange tee</li>
+  <li>brown jeans</li>
+  <li>red shoes</li>
+</ol>
+<h2>extra images</h2>
+<p>all these images use an outdated version of hayden—for the most accurate reference, see the picture at the top!</p>
+<img src="https://stuff.raynec.dev/art/2024/no%20thoughts%2C%20just%20music.jpg" alt="hayden">
+<img src="https://stuff.raynec.dev/art/2024/qwertyy%20pfp.png" alt="hayden">
+<img src="https://stuff.raynec.dev/art/2025/whoami.png" alt="rayne, hayley, and hayden">
