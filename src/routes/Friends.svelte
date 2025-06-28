1d0e64bfd7685139d2ede0db209cf0d6047b29b7
raynecloudy <rayne@raynec.dev>
2025-06-28 12:03:07 -0700

    mov

--- src/routes/Friends.svelte
+++ src/routes/Friends.svelte
@@ -8,6 +8,7 @@
 <p>i have some pretty cool friends :3</p>
 <ul>
   <li><a href="https://elizadoesstuff.nekoweb.org">eliza</a></li>
+  <li><a href="https://man-o-valor.itch.io">mov</a></li>
   <li><a href="https://samv.me">shock59</a></li>
   <li><a href="https://steve0greatness.nekoweb.org">steve0greatness</a></li>
   <li><a href="https://tally.gay">tally</a></li>
